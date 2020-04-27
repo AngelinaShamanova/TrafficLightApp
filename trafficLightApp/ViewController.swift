ebea774b0cd43eab6d63e27006cafbfba9b9e364
Angelina Shamanova <angelinashamanova@gmail.com>
2020-04-27 10:00:05 +0300

    Initial Commit

---
+++ trafficLightApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficLightApp
+//
+//  Created by MacBook on 27/04/2020.
+//  Copyright © 2020 mybestcompany.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
